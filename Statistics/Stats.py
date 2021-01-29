04f2d68fd3c0bd8f7c8b50af6f77cbf62621fb8b
Roger Dev <Roger.Dev@lexisnexisrisk.com>
2021-01-29 16:48:21 -0700

    Finished Stats module for now

--- Statistics/Stats.py
+++ Statistics/Stats.py
@@ -11,6 +11,7 @@ class Stats:
         mins = self.aData.min(1)
         maxs = self.aData.max(1)
         means = self.aData.mean(1)
+        #print('means = ', means)
         stds = self.aData.std(1)
         outDict = {}
         for i in range(self.aData.shape[0]):
@@ -63,8 +64,25 @@ class Stats:
             fieldName = self.fieldList[f]
             #print('discretized = ', discretized.shape, discretized)
             fieldVals = list(discretized[f, :])
+            if len(fieldVals) == 0:
+                return None
             data[fieldName] = fieldVals
         return data
+
+    def getMidpoints(self, field):
+        indx = self.fieldIndex[field]
+        dSpec = self.discSpecs[indx]
+        edges = dSpec[3]
+        mids = []
+        for i in range(len(edges) - 1):
+            mids.append((edges[i] + edges[i+1]) / 2)
+        return mids
+
+    def getBucketVals(self, field):
+        indx = self.fieldIndex[field]
+        dSpec = self.discSpecs[indx]
+        bucketCount = dSpec[0]
+        return range(bucketCount)
 
     def filter(self, filtSpecs):
         #print('self.discretized = ', self.discretized)
@@ -88,11 +106,44 @@ class Stats:
                     break
             if not include:
                 remRecs.append(i)
-        filtered = np.delete(self.discretized, remRecs, 1)
+        filtered = np.delete(self.aData, remRecs, 1)
         #print('filtered = ', filtered.shape,  filtered)
         # Now convert to orginal format, with only ecords that passed filter
         orgFilt = self.toOriginalForm(filtered)
         return orgFilt
+
+    def binToVal(self, field, bin):
+        indx = self.fieldIndex[field]
+        dSpec = self.discSpecs[indx]
+        min = dSpec[1]
+        max = dSpec[2]
+        val = (min + max) / 2
+        return val
+
+    def expected(self, pdf, discrete = False):
+        cum = 0
+        for i in range(len(pdf)):
+            spec = pdf[i]
+            bin, min, max, prob = spec
+            if discrete:
+                value = min
+            else:
+                value = (min + max) / 2
+            cum += prob * value
+        if discrete:
+            exp = round(cum)
+        else:
+            exp = cum
+        return exp
+            
+    def probDisc(self, target, bin):
+        indx = self.fieldIndex[target]
+        dSpec = self.discSpecs[indx]
+        hist = list(dSpec[4])
+        if bin > len(hist) - 1:
+            bin = len(hist) - 1
+        prob = hist[bin] / self.N
+        return prob
 
     def prob(self, target, value):
         indx = self.fieldIndex[target]
@@ -140,6 +191,62 @@ class Stats:
             outPDF.append((i, start, end, outHist[i]))
         return outPDF
 
+    def pdfToProbArray(self, pdf):
+        vals = []
+        for bin in pdf:
+            prob = bin[3]
+            vals.append(prob)
+        outArray = np.array(vals)
+        return outArray
+
+    def condPDF(self, target, givenSpecs):
+        if type(givenSpecs) != type([]):
+            givenSpecs = [givenSpecs]
+        filtSpecs = []
+        condSpecs = []
+        for givenSpec in givenSpecs:
+            field, val = givenSpec
+            # If val is None, we conditionalize.  If a specific value, we filter
+            if val is not None:
+                filtSpecs.append(givenSpec)
+            else:
+                condSpecs.append(givenSpec[0])
+        newData = self.filter(filtSpecs)
+        if not newData:
+            return []
+        filtStats = Stats(newData, self.discSpecs)
+        if len(condSpecs) == 0:
+            outPDF = filtStats.pdf(target)
+            #print('filtStats.discSpecs = ', filtStats.discSpecs)
+        else:
+            accum = None
+            for given in condSpecs:
+                values = self.getMidpoints(given)
+                for value in values:
+                    probGV = self.prob(given, value)
+                    if probGV == 0:
+                        continue
+                    probTGV = filtStats.condPDF(target, (given, value))
+                    if not probTGV:
+                        continue
+                    probs = self.pdfToProbArray(probTGV) * probGV
+                    if accum is None:
+                        accum = probs
+                    else:
+                        accum += probs
+                break
+            template = filtStats.pdf(target)
+            outPDF = []
+            for i in range(len(accum)):
+                pdfBin = template[i]
+                newprob = accum[i]
+                newBin = pdfBin[:-1] + (newprob,)
+                outPDF.append(newBin)
+        return outPDF
+
+    def fieldStats(self, field):
+        return self.fieldAggs[field]
+
     def __init__(self, ds, discSpecs = None):
         self.ds = ds
         self.fieldList = list(ds.keys())
@@ -153,10 +260,11 @@ class Stats:
             npDat.append(ds[field])
         self.aData = np.array(npDat)
         self.N = self.aData.shape[1]
-        self.cardinality = {}
+        #print('N = ', self.N)
         self.fieldAggs = self.getAgg(ds)
         if discSpecs:
             self.discSpecs = self.fixupDiscSpecs(discSpecs)
+            #print('self.aData = ', self.aData)
             self.discretized = self.aData
         else:
             self.discSpecs = self.calcDiscSpecs()
