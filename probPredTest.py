cd2039e79d334ec0d3525ae2e58de9eb43e84561
Roger Dev <Roger.Dev@lexisnexisrisk.com>
2021-03-09 16:55:51 -0700

    Refined probPredTest
    Signed-off-by: Roger Dev <Roger.Dev@lexisnexisrisk.com>

--- probPredTest.py
+++ probPredTest.py
@@ -15,7 +15,7 @@ def run(filename):
     # split data between 'training' and test
     vars = list(dat.keys())
     datLen = len(dat[vars[0]])
-    trainLen = datLen - 10
+    trainLen = datLen - 100
     tr = {}
     te = {}
     for var in dat.keys():
@@ -27,14 +27,16 @@ def run(filename):
     print ('Testing probability module\'s prediction capabilities.')
     ps = ProbSpace(tr, density=1, power=1)
     print()
-    print('Testing Regression with Continuous Variables.')
+    print('Testing non-linear regression with continuous variables.')
     d = ps.distr('Y')
     print('stats(Y) = ', d.mean(), d.stDev(), d.skew(), d.kurtosis())
     # Note: Predict will automatically remove Y from the test data
+    Ymean = d.mean()
     expected = te['Y']
     results = ps.Predict('Y', te)
     #print('results = ', results)
-    cumErr = 0.0
+    SSE = 0.0 # Sum of squared error
+    SST = 0.0 # Sum of squared deviation
     for i in range(len(results)):
         val = results[i]
         exp = expected[i]
@@ -43,9 +45,9 @@ def run(filename):
             X.append(te[x][i])
     
         #print('X = ', X, ', pred = ', val, ', expected = ', exp, ', err = ', val - exp)
-        err2 = (val - exp)**2
-        cumErr += err2
-    print('MSE = ', cumErr / len(results))
+        SSE += (val - exp)**2
+        SST += (exp - Ymean)**2
+    print('R2 = ', 1 - SSE / SST)
     print()
     print('Testing Classification with discontinuous discrete data')
     d = ps.distr('DY')
@@ -61,7 +63,7 @@ def run(filename):
         for x in ['DX1', 'DX2', 'DX3', 'DX4']:
             X.append(te[x][i])
     
-        print('X = ', X, ', pred = ', val, ', expected = ', exp, ', err = ', val != exp)
+        #print('X = ', X, ', pred = ', val, ', expected = ', exp, ', err = ', val != exp)
         if val != exp:
             cumErr += 1
     print('Accuracy = ', 1 - (cumErr / len(results)))
